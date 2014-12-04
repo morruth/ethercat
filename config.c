e43836ec5460eb2edc2f8ce0a781ceb98c82809e
Timur Khanjanov <timur@sip.az>
2014-12-04 05:44:41 +0400

    Add parameters

--- config.c
+++ config.c
@@ -1,8 +1,11 @@
 #include <dotconf.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include "logging.h"
 #include "config.h"
+#include "ether.h"
 
 static DOTCONF_CB(cb_iface);
 static DOTCONF_CB(cb_uid);
@@ -12,75 +15,177 @@ static DOTCONF_CB(cb_etype);
 static DOTCONF_CB(cb_tty);
 static DOTCONF_CB(cb_ttyown);
 static DOTCONF_CB(cb_ttymod);
+static DOTCONF_CB(cb_section);
+static DOTCONF_CB(cb_name);
+static FUNC_ERRORHANDLER(errorhandler);
 
 
 
 static const configoption_t options[] = {
 	{"Interface", ARG_STR, cb_iface, NULL, CTX_ALL},
+	{"Name", ARG_STR, cb_name, NULL, CTX_ALL},
 	{"Uid", ARG_INT, cb_uid, NULL, CTX_ALL},
 	{"Mac", ARG_STR, cb_mac, NULL, CTX_ALL},
 	{"Verbose", ARG_INT, cb_verbose, NULL, CTX_ALL},
 	{"Ethertype", ARG_STR, cb_etype, NULL, CTX_ALL},
 	{"Tty", ARG_TOGGLE, cb_tty, NULL, CTX_ALL},
 	{"Ttyown", ARG_STR, cb_ttyown, NULL, CTX_ALL},
-	{"Ttymod", ARG_IN, cb_ttymod, NULL, CTX_ALL},
+	{"Ttymod", ARG_INT, cb_ttymod, NULL, CTX_ALL},
+	{"[Section]", ARG_NONE, cb_section, NULL, CTX_ALL},
 	LAST_OPTION
 };
 
+static struct etherconf *configs;
+static struct etherconf *config;
+static int sections_idx=0;
 
-static const etherconf config={
-	DEFAULT_ETHER,
-	DEFAULT_UID,
-	NULL,
-	DEFAULT_VERBOSITY,
-	DEFAULT_ETHER_TYPE,
-	DEFAULT_TTY,
-	DEFAULT_TTYOWNER,
-	DEFAULT_TTYGROUP,
-	DEFAULT_TTYRIGHTS
-};
-
-int config_read(char *config_filename){
+struct etherconf *config_read(char *config_filename){
 	configfile_t *configfile;
 
-	configfile = dotconf_create(config_filename ? config_filename : DEFAULT_CONFIG,
+	configfile = dotconf_create(config_filename ? config_filename : DEFAULT_CONFIGFILE,
 				    options, NULL, CASE_INSENSITIVE);
 	if (!configfile) {
-		logit(LOG_ERR, "Error opening config file\n");
-		dotconf_cleanup(configfile);
 		return NULL;
 	}
+	configs=malloc(sizeof(struct etherconf));
+	configs[sections_idx]=defconfig;
+	config=configs;
+	config->name=DEFAULT_SECTION;
+
+        configfile->errorhandler = (dotconf_errorhandler_t) errorhandler;
 
 	if (dotconf_command_loop(configfile) == 0){
 		logit(LOG_ERR, "Error reading config file\n");
 		dotconf_cleanup(configfile);
 		return NULL;
 	}
+	if(config->name == NULL){ /* no name for last section */
+		logit(LOG_ERR,"Section unnamed");
+		return NULL;
+	}
 
-	dotconf_cleanup(configfile);
-	return &config;
+	if(configfile)dotconf_cleanup(configfile);
+	return configs;
 }
 
-
-
-DOTCONF_CB(cb_list)
-{
+struct etherconf *config_section(char *section_name){
 	int i;
-	printf("%s:%ld: %s: [  ",
-	       cmd->configfile->filename, cmd->configfile->line, cmd->name);
-	for (i = 0; i < cmd->arg_count; i++)
-		printf("(%d) %s  ", i, cmd->data.list[i]);
-	printf("]\n");
+	for(i=0;i<=sections_idx;i++){
+	    if(strcmp(configs[sections_idx].name,section_name) == 0 ){
+		return configs+i;
+	    }
+	}
 	return NULL;
 }
 
-DOTCONF_CB(cb_str)
+
+DOTCONF_CB(cb_iface)
 {
-	printf("%s:%ld: %s: [%s]\n",
+/*	printf("%s:%ld: %s: [%s]\n",
 	       cmd->configfile->filename, cmd->configfile->line,
-	       cmd->name, cmd->data.str);
+	       cmd->name, cmd->data.str);*/
+	config->iface=malloc(strlen(cmd->data.str)+1);
+	strcpy(config->iface,cmd->data.str);
 	return NULL;
 }
+
+DOTCONF_CB(cb_uid)
+{
+	config->work_uid=cmd->data.value;
+	return NULL;
+}
+
+DOTCONF_CB(cb_verbose)
+{
+	config->verbose=cmd->data.value;
+	return NULL;
+}
+
+DOTCONF_CB(cb_tty)
+{
+	config->pseudo_tty=cmd->data.value;
+	return NULL;
+}
+
+DOTCONF_CB(cb_ttymod)
+{
+	config->rights=cmd->data.value;
+	return NULL;
+}
+
+
+DOTCONF_CB(cb_ttyown)
+{
+	uid_t owner;
+	gid_t group;
+	int count=0;
+	count=sscanf(cmd->data.str," %d:%d ",&owner,&group);
+	if (count == 2){
+		config->owner=owner;
+		config->group=group;
+	}else{
+		return "Wrong tty owner";
+	}
+
+	return NULL;
+}
+
+DOTCONF_CB(cb_etype)
+{
+	if(sscanf(cmd->data.str," %04hx ",&config->ether_type)==1){
+		return NULL;
+	}else {
+		return "Wrong ethernet type";
+	}
+}
+
+
+
+FUNC_ERRORHANDLER(errorhandler)
+{
+        logit(LOG_ERR,"Config Error: %s", msg);
+        return 0;
+}
+
+DOTCONF_CB(cb_section)
+{
+	struct etherconf *new_configs;
+	if(configs[sections_idx].name == NULL ){
+		/* No name in previous section */
+		return "Section unnamed";
+	}
+	sections_idx++;
+	new_configs=realloc(configs,sizeof(struct etherconf)*(sections_idx+1));
+	if( errno == ENOMEM){
+		return "Memory allocation for section failed";
+	}
+	if( new_configs != configs ) {
+		free(configs);
+		configs=new_configs;
+	}
+	configs[sections_idx]=defconfig;
+	config=configs+sections_idx;
+
+	return NULL;
+}
+
+DOTCONF_CB(cb_name)
+{
+	config->name=malloc(strlen(cmd->data.str)+1);
+
+	return NULL;
+}
+
+DOTCONF_CB(cb_mac)
+{
+	config->mac=ether_aton(cmd->data.str);
+        if(NULL == config->mac) {
+		return "Wrong MAC format\n";
+        }
+
+	return NULL;
+}
+
 
 /*
   vim:set ts=4:
--- config.h
+++ config.h
@@ -1,3 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define	DEFAULT_ETHER_TYPE 0x9600 /*default ethernet type is com1 port most often used speed ;)*/
 #define	DEFAULT_ETHER	"eth0"
+#define DEFAULT_UID 65535
+#define DEFAULT_VERBOSITY 0
+#define DEFAULT_TTY 0
+#define DEFAULT_TTYOWNER 10 /* uucp FIXME!!! Debianism*/
+#define DEFAULT_TTYGROUP 10
+#define DEFAULT_TTYRIGHTS 0660
+#define DEFAULT_CONFIGFILE "/etc/ethercat.conf"
+#define DEFAULT_SECTION "default"
+
+
+#define bool int
+
+struct etherconf {
+	char *name;
+	char *iface;
+	uid_t work_uid;
+	struct ether_addr *mac;
+	int verbose;
+	u_int16_t ether_type;
+	bool pseudo_tty;
+	uid_t owner;
+	gid_t group;
+	mode_t rights;
+};
+
+
+static struct etherconf defconfig={
+	NULL,
+	DEFAULT_ETHER,
+	DEFAULT_UID,
+	NULL,
+	DEFAULT_VERBOSITY,
+	DEFAULT_ETHER_TYPE,
+	DEFAULT_TTY,
+	DEFAULT_TTYOWNER,
+	DEFAULT_TTYGROUP,
+	DEFAULT_TTYRIGHTS
+};
+
+struct etherconf *config_read(char *config_filename);
+struct etherconf *config_section(char *config_section);
